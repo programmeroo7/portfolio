d995005733f9e0e16c0cde3e4e6a8871fee90e22
programmeroo7 <rk8670629280@gmail.com>
2019-12-29 00:10:57 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/programmeroo7",
+    "name": "Rohit",
+    "role": "Full Stack Developer",
+    "linkedinId": "rk8679629280@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rohit-kumar-303990117/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/programmeroo7",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/Rohit86799",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently working as a full stack web developer in Rexweb Solutions.",
+    "address":"Phase 1 Mohali PB (160055)",
+    "website":"https://github.com/programmeroo7",
+    "education":[
+      {
+        "UniversityName":"Govt. Polytechnic hamirpur",
+        "specialization":"Diploma in Computer Engineering",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2016",
+        "Achievements":""
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Rexweb Solutions Company",
+        "specialization":"Full Stack",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2019",
+        "Achievements":"Employee of the mont in oct 2018"
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"React Native"
+      },
+      {
+        "skillname":"html5"
+      },
+      {
+        "skillname":"jquery"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Effi",
+        "description":"effie> is a service for system brand and sales management on store shelves, where the first moment of truth occurs: the buyer makes a choice and the product becomes a purchase. Developed and based on advanced technologies, simple and easy to use - the mobile employee starts work on the route by pressing the effie> button.",
+        "imgurl":"images/portfolio/effie.jpg"
+      },
+      // {
+      //   "name":"OZ Live",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/project.jpg"
+      // },
+      {
+        "name":"Suburbrun",
+        "description":"Suburb Run is a collection of courses that we have created for our community. We don’t hold competitions or events, we simply provide you with the course maps to explore areas and have fun doing so. We don’t mind if you walk, jog or run these courses - that is entirely up to you. It’s about getting out and about and being active. Feel free to bring a friend along and explore together, or even challenge each other to go further or faster. How you approach the courses is your choice.",  
+        "imgurl":"images/portfolio/suburbrun.png"
+      },
+      {
+        "name":"Usmlebank",
+        "description":"USMLEBANK is commited to buidling a quality question bank and test prep system geared towards making you the best possible doctors you can be!",
+        "imgurl":"images/portfolio/usmlebank.png"
+        //https://react-datepisker-slider.herokuapp.com/
+      },
+      {
+        "name":"Comtestmed",
+        "description":"COMTEST is not for everyone, if you want to waste time studying and being inefficient there are many other educational sources out there. Great doctors prepare with COMTEST, let us help you! Your test is coming soon, so let's work together and the test makers won't stand a chance!",
+        "imgurl":"images/portfolio/comtestmed.png"
+        //https://react-datepisker-slider.herokuapp.com/
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Very good guy for react js",
+        "name":"Steafan"
+      },
+      {
+        "description":"Always happy to have such a great help in my team.",
+        "name":"Amol Malankar"
+      },
+      {
+        "description":"Very dedicated person in future i will definitly want to enjoy work with you!",
+        "name":"Cris"
+      },
+      {
+        "description":"smart guy...",
+        "name":"John Smith"
+      }
+    ]
+  }
+  
+  export default resumeData
